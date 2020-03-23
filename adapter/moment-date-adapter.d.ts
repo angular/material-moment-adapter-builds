69f97f8d57a4eb44a3b3dd61a5131b77c9d2c2c0
mmalerba <mmalerba@google.com>
2020-03-23 22:14:07 +0000

    master - 16e2cf094 Revert "refactor: change isDateInstance return type to is D" (#18897)

--- adapter/moment-date-adapter.d.ts
+++ adapter/moment-date-adapter.d.ts
@@ -7,7 +7,6 @@
  */
 import { InjectionToken } from '@angular/core';
 import { DateAdapter } from '@angular/material/core';
-import * as _moment from 'moment';
 import { Moment } from 'moment';
 /** Configurable options for {@see MomentDateAdapter}. */
 export interface MatMomentDateAdapterOptions {
@@ -58,7 +57,7 @@ export declare class MomentDateAdapter extends DateAdapter<Moment> {
      * string into null. Returns an invalid date for all other values.
      */
     deserialize(value: any): Moment | null;
-    isDateInstance(obj: any): obj is _moment.Moment;
+    isDateInstance(obj: any): boolean;
     isValid(date: Moment): boolean;
     invalid(): Moment;
     /** Creates a Moment instance while respecting the current UTC settings. */
